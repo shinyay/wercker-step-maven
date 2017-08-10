fee2e3dbaecc138b83f963c713388a8de24fe394
shinyay <shinya.com@gmail.com>
2017-08-10 09:04:38 +0900

    Add Maven commnd script

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+### Check if Maven is installed
+if ! type mvn >/dev/null 2>&1; then
+  error "Maven is not installed"
+else
+  info "Maven is installed"
+  info "$(type mvn)"
+  debug "$(mvn -version)"
+fi
+
+### Check if Maven Goal is configured
+if [[ -z $WERCKER_MVN_GOALS ]]; then
+  error "Maven Goal is not configured"
+fi
+
+run() {
+  mvn \
+    --update-snapshots \
+    --batch-mode \
+    -Dmaven.repo.local=${WERCKER_CACHE_DIR} \
+    ${WERCKER_MVN_GOALS}"
+}
+
+run
